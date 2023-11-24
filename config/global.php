fcb32dd355a1c7c50099c84dca1e063de3c1dde1
keylor <91291890+KeyTC@users.noreply.github.com>
2023-11-24 12:40:54 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,15 +1,15 @@
 <?php 
 //Ip de la pc servidor de base de datos
-define("DB_HOST","localhost");
+define("DB_HOST","monorail.proxy.rlwy.net");
 
 //Nombre de la base de datos
-define("DB_NAME", "bdescuela");
+define("DB_NAME", "railway");
 
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "5fFHhAGBefEBDbe1C14FGA5GG224HE2f");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
